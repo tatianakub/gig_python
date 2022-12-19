f1c4049a1a6e7d9e99bd115a2d997c6ad0e9d382
tatianakub <pershik86@mail.ru>
2022-12-19 14:53:10 +0300

    Add lesson_2

--- lesson_1.py
+++ lesson_1.py
@@ -42,4 +42,8 @@
 # 5. Напишите программу, которая принимает на вход число и проверяет,
 # кратно ли оно 5 и 10 или 15, но не 30.
 
-
+num = int(input("Input your number: "))
+if (num % 5 == 0 and num % 10 == 0 or num % 15 == 0) and num % 30 != 0:
+    print("Yes")
+else:
+    print("No")    
